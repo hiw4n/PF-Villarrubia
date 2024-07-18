c423f5104bcf53071b2639a1d6417d24ff5628cd
hiw4n <terriblehiwan@gmail.com>
2024-07-18 19:04:08 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { CoursesComponent } from './features/dashboard/courses/courses.component';
+import { StudentsComponent } from './features/dashboard/students/students.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*  {
+    path: 'auth',
+    component: LoginComponent,
+  }, */
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'courses',
+        component: CoursesComponent,
+      },
+      {
+        path: 'students',
+        component: StudentsComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'students',
+        component: StudentsComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
