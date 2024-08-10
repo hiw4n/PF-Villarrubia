a1172ae9e16c015be1195b2727922b835880aa90
hiw4n <terriblehiwan@gmail.com>
2024-08-10 22:08:04 +0200

    validate spec input required in login

--- src/app/features/auth/login/login.component.spec.ts
+++ src/app/features/auth/login/login.component.spec.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './login.component';
 import { AuthService } from '../../../core/services/auth.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from '../../../shared/material/material.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import {
   BrowserAnimationsModule,
   NoopAnimationsModule,
@@ -29,9 +29,27 @@ describe('TEST DE LOGIN -> LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    /* form = new LoginComponent(new FormBuilder()); */
   });
 
   it('should create, with all imports modules 😅', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Form have form input email', () => {
+    expect(component.loginForm.contains('email')).toBeTruthy();
+  });
+  it('email is required', () => {
+    const control = component.loginForm.get('email');
+    control?.setValue('');
+    expect(control?.valid).toBeFalsy();
+  });
+  it('Form have form input password', () => {
+    expect(component.loginForm.contains('password')).toBeTruthy();
+  });
+  it('password is required', () => {
+    const control = component.loginForm.get('password');
+    control?.setValue('');
+    expect(control?.valid).toBeFalsy();
   });
 });
