12510fffa800e7fa47814a024b9b8756a6b2be58
hiw4n <terriblehiwan@gmail.com>
2024-08-10 18:53:08 +0200

    borrado de los spec para evitar notificaciones en Jasmine

--- src/app/shared/pipes/studen-name-lastname.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StudenNameLastnamePipe } from './studen-name-lastname.pipe';
-
-describe('StudenNameLastnamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new StudenNameLastnamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
